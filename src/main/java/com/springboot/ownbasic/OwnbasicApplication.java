d5a2e5d580bd986eb71342ad07b5af994a54a55e
quanmeng <quanmeng1013@126.com>
2020-04-13 14:02:40 +0800

    测试

---
+++ src/main/java/com/springboot/ownbasic/OwnbasicApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.ownbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnbasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnbasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/ownbasic/OwnbasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.ownbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnbasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
